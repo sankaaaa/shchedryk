92d7db636ca5c4eeda7401c2ed7a68f600248060
sankaaaa <olexa2404@gmail.com>
2024-05-23 02:01:53 +0300

    жити уже не хочеться (нав|меню|мейн)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// App.js
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Singers from "./pages/Singers";
 import Directors from "./pages/Directors";
+import MainPage from "./pages/MainPage";
 import './App.css';
 
 function App() {
@@ -9,12 +11,17 @@ function App() {
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
+        if (!isOpen) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
     };
 
     return (
         <BrowserRouter>
             <nav>
-                <h2 className="title">SHCHEDRYK</h2>
+                <Link to="/" className="title">SHCHEDRYK</Link>
                 <div className={`menu-toggle ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
                     <div className="hamburger"></div>
                 </div>
@@ -23,9 +30,11 @@ function App() {
                 <Link to="/singers" onClick={toggleMenu}>Singers</Link>
                 <Link to="/directors" onClick={toggleMenu}>Directors</Link>
             </div>
+            <div className={`menu-overlay ${isOpen ? 'open' : ''}`}></div> {/* Додаємо .menu-overlay */}
             <Routes>
                 <Route path="/singers" element={<Singers />} />
                 <Route path="/directors" element={<Directors />} />
+                <Route path="/" element={<MainPage />}/>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/MainPage.js
@@ -0,0 +1,27 @@
+import "../styles/main-page.css";
+const MainPage = () => {
+    return (
+        <div className="container">
+            <div className="background-image"></div>
+            <div className="content">
+                <section className="section">
+                    <h1>Section 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac ex vel mauris gravida bibendum.</p>
+                </section>
+                <section className="section">
+                    <h1>Section 2</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac ex vel mauris gravida bibendum.</p>
+                </section>
+                <section className="section">
+                    <h1>Section 3</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac ex vel mauris gravida bibendum.</p>
+                </section>
+                <section className="section">
+                    <h1>Section 4</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac ex vel mauris gravida bibendum.</p>
+                </section>
+            </div>
+        </div>
+    );
+}
+export default MainPage;
